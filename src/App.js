b8a5d186baf6d9e9c7f8aa608d4430ebe8a87fa8
daniel <dev@digitalrepublic.com.br>
2019-07-19 16:19:46 -0300

    Player

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { Fragment } from "react";
 import GlobalStyle from "./styles/global";
 
 import Sidebar from "./components/Sidebar";
+import Player from "./components/Player";
+
+import { Wrapper, Content } from "./styles/components";
 
 import "./styles/global";
 
 const App = () => (
   <Fragment>
     <GlobalStyle />
-    <Sidebar />
+    <Wrapper>
+      <Sidebar />
+      <Player />
+    </Wrapper>
   </Fragment>
 );
 
---
+++ src/components/Player/index.js
@@ -0,0 +1,78 @@
+import React from "react";
+import Slider from "rc-slider";
+
+import {
+  Container,
+  Current,
+  Volume,
+  Progress,
+  Controls,
+  Time,
+  ProgressSlider
+} from "./styles";
+
+import VolumeIcon from "../../assets/images/volume.svg";
+import ShuffleIcon from "../../assets/images/shuffle.svg";
+import BackwardIcon from "../../assets/images/backward.svg";
+import PlayIcon from "../../assets/images/play.svg";
+import ForwardIcon from "../../assets/images/forward.svg";
+import RepearIcon from "../../assets/images/repeat.svg";
+
+const Player = () => (
+  <Container>
+    <Current>
+      <img
+        src="https://f4.bcbits.com/img/a2939269798_10.jpg"
+        alt="Album de musica"
+      />
+
+      <div>
+        <span>Times like these</span>
+        <small>Foo Fighters</small>
+      </div>
+    </Current>
+
+    <Progress>
+      <Controls>
+        <button>
+          <img src={ShuffleIcon} alt="Shuffle" />
+        </button>
+        <button>
+          <img src={BackwardIcon} alt="BackwardIcon" />
+        </button>
+        <button>
+          <img src={PlayIcon} alt="PlayIcon" />
+        </button>
+        <button>
+          <img src={ForwardIcon} alt="ForwardIcon" />
+        </button>
+        <button>
+          <img src={RepearIcon} alt="RepearIcon" />
+        </button>
+      </Controls>
+
+      <Time>
+        <span>1:39</span>
+        <ProgressSlider>
+          <Slider
+            railStyle={{ background: "#404040", borderRadius: 10 }}
+            trackStyle={{ background: "#1ED760" }}
+            handleStyle={{ border: 0 }}
+          />
+        </ProgressSlider>
+      </Time>
+    </Progress>
+
+    <Volume>
+      <img src={VolumeIcon} alt="Volume" />
+      <Slider
+        railStyle={{ background: "#404040", borderRadius: 10 }}
+        trackStyle={{ background: "#FFF" }}
+        handleStyle={{ display: "none" }}
+        value={100}
+      />
+    </Volume>
+  </Container>
+);
+
+export default Player;
