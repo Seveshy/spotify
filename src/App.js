8ff1decd373512967329f661b03a467fb465fade
daniel <dev@digitalrepublic.com.br>
2019-07-22 11:57:32 -0300

    Estilizando Playlist

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React, { Fragment } from "react";
 import GlobalStyle from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
 
 import Sidebar from "./components/Sidebar";
 import Player from "./components/Player";
 import Header from "./components/Header";
+
+import Routes from "./routes";
 
 import { Wrapper, Content, Container } from "./styles/components";
 
 import "./styles/global";
 
 const App = () => (
-  <Fragment>
-    <GlobalStyle />
-    <Wrapper>
-      <Container>
-        <Sidebar />
-        <Content>
-          <Header />
-        </Content>
-      </Container>
-      <Player />
-    </Wrapper>
-  </Fragment>
+  <BrowserRouter>
+    <Fragment>
+      <GlobalStyle />
+      <Wrapper>
+        <Container>
+          <Sidebar />
+          <Content>
+            <Header />
+            <Routes />
+          </Content>
+        </Container>
+        <Player />
+      </Wrapper>
+    </Fragment>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/pages/browse/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+import { Container, Title, List, Playlist } from "./styles";
+
+const Browse = () => (
+  <Container>
+    <Title>Navegar</Title>
+    <List>
+      <Playlist href="">
+        <img
+          src="https://f4.bcbits.com/img/a2939269798_10.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe em quanto você programa ouvindo apenas as melhores do rock
+          nacional!
+        </p>
+      </Playlist>
+
+      <Playlist href="">
+        <img
+          src="https://f4.bcbits.com/img/a2939269798_10.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe em quanto você programa ouvindo apenas as melhores do rock
+          nacional!
+        </p>
+      </Playlist>
+
+      <Playlist href="">
+        <img
+          src="https://f4.bcbits.com/img/a2939269798_10.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe em quanto você programa ouvindo apenas as melhores do rock
+          nacional!
+        </p>
+      </Playlist>
+
+      <Playlist href="">
+        <img
+          src="https://f4.bcbits.com/img/a2939269798_10.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe em quanto você programa ouvindo apenas as melhores do rock
+          nacional!
+        </p>
+      </Playlist>
+    </List>
+  </Container>
+);
+
+export default Browse;
