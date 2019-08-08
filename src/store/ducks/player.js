1abd84e9e32e825eef2e68304810a5bc27fe8645
daniel <dev@digitalrepublic.com.br>
2019-08-08 15:59:37 -0300

    Exibindo tempo de duraçao / Volume

--- src/store/ducks/player.js
+++ src/store/ducks/player.js
@@ -6,7 +6,10 @@ export const Types = {
   PAUSE: "player/PAUSE",
   NEXT: "player/NEXT",
   PREV: "player/PREV",
-  PLAYING: "player/PLAYING"
+  PLAYING: "player/PLAYING",
+  HANDLE_POSITION: "player/HANDLE_POSITION",
+  SET_POSITION: "player/SET_POSITION",
+  SET_VOLUME: "player/SET_VOLUME"
 };
 
 const INITIAL_STATE = {
@@ -14,7 +17,9 @@ const INITIAL_STATE = {
   list: [],
   status: Sound.status.PLAYING,
   position: null,
-  duration: null
+  positionShown: null,
+  duration: null,
+  volume: 100
 };
 
 export default function player(state = INITIAL_STATE, action) {
@@ -37,7 +42,12 @@ export default function player(state = INITIAL_STATE, action) {
       const prev = state.list[currentIndex - 1];
 
       if (prev) {
-        return { ...state, currentSong: prev, status: Sound.status.PLAYING };
+        return {
+          ...state,
+          currentSong: prev,
+          status: Sound.status.PLAYING,
+          position: 0
+        };
       }
 
       return state;
@@ -49,13 +59,31 @@ export default function player(state = INITIAL_STATE, action) {
       const next = state.list[currentIndex + 1];
 
       if (next) {
-        return { ...state, currentSong: next, status: Sound.status.PLAYING };
+        return {
+          ...state,
+          currentSong: next,
+          status: Sound.status.PLAYING,
+          position: 0
+        };
       }
 
       return state;
     }
     case Types.PLAYING:
       return { ...state, ...action.payload };
+    case Types.HANDLE_POSITION:
+      return {
+        ...state,
+        positionShown: state.duration * action.payload.percent
+      };
+    case Types.SET_POSITION:
+      return {
+        ...state,
+        position: state.duration * action.payload.percent,
+        positionShown: null
+      };
+    case Types.SET_VOLUME:
+      return { ...state, volume: action.payload.volume };
     default:
       return state;
   }
@@ -78,5 +106,19 @@ export const Creators = {
   playing: ({ position, duration }) => ({
     type: Types.PLAYING,
     payload: { position, duration }
+  }),
+
+  handlePosition: percent => ({
+    type: Types.HANDLE_POSITION,
+    payload: { percent }
+  }),
+  setPosition: percent => ({
+    type: Types.SET_POSITION,
+    payload: { percent }
+  }),
+
+  setVolume: volume => ({
+    type: Types.SET_VOLUME,
+    payload: { volume }
   })
 };
